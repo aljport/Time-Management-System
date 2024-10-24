a07dbdc4c23f2f710ee40b07f0fd1dd135c2d5f6
Alyssa Eriksen <aeriksen@ufl.edu>
2024-10-24 16:16:37 -0400

    views, utilities, models, forms file additions

--- backend/website/OnSchedule/models.py
+++ backend/website/OnSchedule/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Calendar_Event(models.Model):
+    # contains title, desc, start/end time vars, etc for the event
+    title = models.CharField(max_length=150)
+    description = models.TextField(blank=True)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    user_created = models.ForeignKey(User, on_delete=models.CASCADE) 
+    attendee = models.ManyToManyField(User, related_name='events', blank=True)
+
+    def __str__(self):
+        return self.title # string of event 
+    
+class Notifications(models.Model):
+    # links both event and user who get notif / what notif is 
+    event = models.ForeignKey(Calendar_Event, on_delete=models.CASCADE)
+    user = models.ForeignKey(Calendar_Event, on_delete=models.CASCADE)
+    time_sent = models.DateTimeField(auto_now_add=True) 
+    notified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Notification for {self.user.username} about {self.event.title}'  
+    
+
--- backend/website/OnSchedule/views.py
+++ backend/website/OnSchedule/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from .models import Event
+from .forms import EventForm
+from .models import User
+from .utilities import save_user
 
-# Create your views here.
+
+def create_event(ev_req):
+    if ev_req.method == 'POST':
+        form = EventForm(ev_req.POST) 
+        if form.is_valid():
+            form.save()
+            return redirect('event_list')
+    else:
+        form = EventForm() # creates empty form 
+
+    return render(ev_req, 'create_event.html', {'form': form})
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            save_user(form.cleaned_data)
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'register.html', {'form': form})
