37c288cb648a990dd996f02636327353496a13e5
Alyssa Eriksen <aeriksen@ufl.edu>
2024-10-25 21:44:34 -0400

    more change
    fleshed out the create_event function, removed the register user (i believe already implemented) and defined a create_notifications function that hopefully creates the notification object

--- backend/website/OnSchedule/views.py
+++ backend/website/OnSchedule/views.py
@@ -2,29 +2,34 @@ from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from .models import Event
 from .forms import EventForm
-from .models import User
+from .models import User, Notifications 
 from .utilities import save_user
+from django.utils import timezone
 
 
 def create_event(ev_req):
     if ev_req.method == 'POST':
         form = EventForm(ev_req.POST) 
         if form.is_valid():
-            form.save()
+            event = form.save(commit=False)
+            event.user_created = ev_req.user
+            event.save()
             return redirect('event_list')
+        
+        attendees = form.cleaned_data.get('attendee')
+        if attendees:
+            event.attendee.set(attendees)
+
+        create_notifications(event, attendees)
+
     else:
         form = EventForm() # creates empty form 
 
     return render(ev_req, 'create_event.html', {'form': form})
 
-
-def register_user(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            save_user(form.cleaned_data)
-            return redirect('login')
-    else:
-        form = UserCreationForm()
-
-    return render(request, 'register.html', {'form': form})
+def create_notifications(event, attendees):
+    notifications = [   
+        Notifications(event=event, user=attendee)
+        for attendee in attendees
+    ]
+    Notifications.objects.bulk_create(notifications)
--- backend/website/website/urls.py
+++ backend/website/website/urls.py
@@ -1,13 +1,9 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib import admin
 from django.urls import path
-# from .views import create_event
-# from .views import register_user
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('create/', create_event, name='create_event'),
-    # path('register/', register_user, name="register"),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
