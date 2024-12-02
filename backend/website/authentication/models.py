85f097eb7f11b6a0927d05cc27a94f81840f0fe4
Dylon Michaud <115090575+GrDSM@users.noreply.github.com>
2024-12-02 15:20:02 -0500

    Delete backend/website directory

--- backend/website/authentication/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-# Create your models here.
-class Friends(models.Model):
-    Amigo = models.ForeignKey(User, on_delete=models.CASCADE)
-    sharesCalendar = models.BooleanField(default = False)
